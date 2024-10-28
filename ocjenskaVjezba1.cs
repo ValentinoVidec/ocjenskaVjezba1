2f6b68dedc858cc9957366ec0c1e998ed1c31f49
ValentinoVidec <valentinovidec1505@gmail.com>
2024-10-28 12:42:29 +0100

    Add files via upload

---
+++ ocjenskaVjezba1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ocjenskaVjezba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite string:");
+            string input = Console.ReadLine();
+
+            if (input.Length < 5)
+            {
+                Console.WriteLine("Rezultat: " + input);
+            }
+            else
+            {
+                string result = input.Substring(2, input.Length - 4);
+                Console.WriteLine("Rezultat: " + result);
+            }
+            Console.ReadKey();
+        }
+    }
+}
